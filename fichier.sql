8b2d11505e403343593ace5fbce3ef3021f2b49d
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2024-04-23 15:28:07 +0000

    Nouveaux fichiers

--- fichier.sql
+++ fichier.sql
@@ -29,3 +29,21 @@ ALTER TABLE Product ADD Category VARCHAR(20);
 ALTER TABLE Orders ADD OrderDate DATE;
 
 ALTER TABLE Orders ALTER OrderDate SET DEFAULT (SYSDATE());
+
+/* ---------- INSERTION DES DONNEES DANS LES TABLES ---------- */
+
+INSERT INTO `Customer` (`Customer_id`, `Customer_Name`, `Customer_Tel`) 
+VALUES
+    ('C01', 'ALI', '71321009'),
+    ('C02', 'ASMA', '77345823');
+
+INSERT INTO `Product` (`Product_id`, `Product_Name`, `Category`, `Price`) 
+VALUES
+    ('P01', 'Samsung Galaxy S20', 'Smartphone', '3299'),
+    ('P02', 'ASUS Notebook', 'PC', '4599');
+
+INSERT INTO `Orders` (`Customer_id`, `Product_id`, `OrderDate`, `Quantity`, `Total_amount`) 
+VALUES
+    ('C01', 'P01', NULL, '2', '9198'),
+    ('C02', 'P02', '2020-05-28', '1', '3299');
+
